6eb3527ef3e8665ebeadbfd4a5a9fb4c3cfdc2b9
Hatice Özbolat <74445249+haticeozbolat01@users.noreply.github.com>
2025-05-29 13:44:19 +0300

    Code Python Project

---
+++ Band Name Generator Project.py
@@ -0,0 +1,6 @@
+#Bugün Python'daki ilk adımlarımızdan birini atacağız: ekrana yazı yazdırmak ve kullanıcıdan bilgi almak! Merak etmeyin, hiç de karmaşık değil. Bu iki sihirli değnek gibi fonksiyonumuz var:
+
+#print(): Adı üstünde, bu fonksiyonla Python'a "Bunu ekrana yazdır!" diyebiliriz. Mesajları, sayıları, her şeyi gösterebiliriz. Tıpkı bir konuşma baloncuğu gibi düşünün.
+# input(): Bu da bizim için kullanıcıyla etkileşim kurmanın yolu. Kullanıcıya bir soru sorup, onun klavyeden girdiği cevabı almamızı sağlar. Yani, "Ne düşünüyorsun?" diye sorup cevabı dinlemek gibi!
+# Şimdi gelin, bu iki fonksiyonu kullanarak projeniz için harika bir grup ismi belirleyelim.
+
